7316b7394c30b128910d1e02f76e1c9a473ed7ef
Jean <jeanccald@gmail.com>
2021-02-18 08:23:17 -0400

    Level 1

---
+++ Level 1/level1.js
@@ -0,0 +1,21 @@
+let myName = "Jean"
+let mentorName = "Rafael"
+
+console.log(myName == mentorName);
+
+let integerVariable = 1;
+let stringVariable = '1';
+
+console.log(integerVariable == stringVariable);
+
+let type_number = 1;
+let type_string = "hi";
+let type_boolean = false;
+let type_object = {};
+let type_function = new Function();
+
+console.log(typeof type_number);
+console.log(typeof type_string);
+console.log(typeof type_boolean);
+console.log(typeof type_object);
+console.log(typeof type_function);
